64ec279f3a6ace5dd93b47343bb2dedf6dd9404f
nehabalaji <55323591+nehabalaji@users.noreply.github.com>
2019-09-29 16:26:50 +0530

    Add files via upload

--- stud.java
+++ stud.java
@@ -1,23 +1,39 @@
+package marks;
+
 import java.util.Scanner;
 
 public class stud {
-
-	public void display(String a, int b, int c) {
-		System.out.println("student name: \t"+a+"\n roll no. :\t"+b+"\n class:\t"+c);
+	
+	public int totalmarks(int a, int b, int c, int d, int e,int f) {
+	
+	return (a+b+c+d+e+f);
+	}
+	
+	public float average(int a, int b, int c, int d, int e, int f) {
+		float avg=(float)(a+b+c+d+e+f)/6;
+		return avg;
 	}
 	
 	
-	public static void main(String[] args) {
-		String n;
-		int r, c;
-		Scanner sc= new Scanner(System.in);
+	public static void main(String[] args){
+		int s1, s2, s3, s4, s5, s6;
+		
+		Scanner sc=new Scanner(System.in);
 		stud b= new stud();
-		n=sc.next();
-		r=sc.nextInt();
-		c=sc.nextInt();
 		
-		b.display(n, r, c);
+		s1=sc.nextInt();
+		s2=sc.nextInt();
+		s3=sc.nextInt();
+		s4=sc.nextInt();
+		s5=sc.nextInt();
+		s6=sc.nextInt();
+		
+		int total = b.totalmarks(s1, s2, s3, s4, s5, s6);
+		
+		float avrg= b.average(s1, s2, s3, s4, s5, s6);
 		
 		
+		System.out.println("total:\t"+total+"\n average:\t"+avrg);
 	}
+
 }
